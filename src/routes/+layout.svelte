7b6e50458801cf3475b612b176ba20d8767b2d31
Hunter Johnston <johnstonhuntera@gmail.com>
2022-08-30 18:20:29 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="container mx-auto px-4 py-4">
+	<nav>
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+	</nav>
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col pt-4 justify-center items-center">
+	<h1 class="text-2xl font-medium">Device Selector</h1>
+	<h2 class="text-3xl py-6">What kind of device are you looking for?</h2>
+	<div class="flex">
+		<button class="btn-primary mx-4"> Router </button>
+		<button class="btn-primary mx-4"> Switch </button>
+		<button class="btn-primary mx-4"> Firewall </button>
+	</div>
+</div>
