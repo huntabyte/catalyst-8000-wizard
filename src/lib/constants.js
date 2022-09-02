7226e65999857bf096e370faf22d15d2b7ea4f01
Hunter Johnston <johnstonhuntera@gmail.com>
2022-09-01 22:30:33 -0400

    results page done

---
+++ src/lib/constants.js
@@ -0,0 +1,49 @@
+export const constants = {
+	'10g_port_density': '10GE Port Density',
+	'1g_port_density': '1GE Port Density',
+	sm_slots: 'Service Module Slots',
+	nim_slots: 'Network Interface Module Slots',
+	pim_slots: 'Pluggable Interface Module Slots',
+	memory_default: 'Default Memory',
+	storage_default: 'Default Storage',
+	ipv4_forwarding: 'IPv4 Forwarding',
+	ipsec_throughput: 'IPsec Throughput',
+	ipsec_svti_tunnels: 'IPsec SVTI Tunnels',
+	acls: 'Access Control Lists (ACLs)',
+	ipv4_aces: 'IPv4 ACEs',
+	ipv4_routes: 'IPv4 Routes',
+	ipv6_routes: 'IPv6 Routes',
+	queues: 'Queues',
+	nat_sessions: 'NAT Sessions',
+	fw_sessions: 'Firewall Sessions',
+	vrfs: 'VRFs'
+};
+
+export const specs = {
+	'10g_port_density': '10GE Port Density',
+	'1g_port_density': '1GE Port Density',
+	sm_slots: 'Service Module Slots',
+	nim_slots: 'Network Interface Module Slots',
+	pim_slots: 'Pluggable Interface Module Slots',
+	memory_default: 'Default Memory',
+	storage_default: 'Default Storage'
+};
+
+export const performance = {
+	ipv4_forwarding: 'IPv4 Forwarding',
+	ipsec_throughput: 'IPsec Throughput',
+	ipsec_svti_tunnels: 'IPsec SVTI Tunnels'
+};
+
+export const scalability = {
+	acls: 'Access Control Lists (ACLs)',
+	ipv4_aces: 'IPv4 ACEs',
+	ipv4_routes: 'IPv4 Routes',
+	ipv6_routes: 'IPv6 Routes',
+	queues: 'Queues',
+	nat_sessions: 'NAT Sessions',
+	fw_sessions: 'Firewall Sessions',
+	vrfs: 'VRFs'
+};
+
+export default constants;
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import devices from '$lib/data/devices.json';
+	import constants, { specs, performance, scalability } from '$lib/constants.js';
+
 	let results = devices.filter((device) => device.ru == 2);
 
 	let quantity = results.length;
@@ -8,42 +10,74 @@
 <div class="w-full h-full pt-8">
 	<div class="flex flex-col items-center h-full w-full mx-auto">
 		<h1 class="text-5xl font-medium pb-8">Results</h1>
-		<div class="card w-11/12 bg-white shadow-xl min-h-max h-full">
+		<div class="card w-11/12 bg-white shadow-xl h-auto pb-8">
 			<div class="grid grid-cols-3 divide-x h-48">
 				<div class="w-full h-full px-6" />
-				<div class="w-full h-full px-6 py-6">
-					<div class="flex flex-col h-auto justify-between">
-						<div>
-							<h1 class="font-medium text-center text-xl">C8300-1N1S-4T2X</h1>
-							<p class="text-center">Description of device will go here</p>
-						</div>
-						<div class="pt-4">
-							<img src="img/C8300-1N1S-4T2X.webp" alt=" One" />
-						</div>
-					</div>
-				</div>
-				<div class="w-full h-full px-6 py-6">
-					<div class="flex flex-col h-auto justify-between">
-						<div>
-							<h1 class="font-medium text-center text-xl">C8300-2N2S-4T2X</h1>
-							<p class="text-center">Description of device will go here</p>
-						</div>
-						<div class="pt-4">
-							<img src="img/C8300-2N2S-4T2X.webp" alt=" One" />
+				{#each results as result}
+					<div class="w-full h-full px-6 py-6">
+						<div class="flex flex-col h-auto justify-between">
+							<div>
+								<h1 class="font-medium text-center text-xl">{result.part_number}</h1>
+								<p class="text-center text-xs pt-2">{result.description}</p>
+							</div>
+							<div class="pt-4">
+								<img src="img/{result.part_number}.webp" alt=" One" />
+							</div>
 						</div>
 					</div>
-				</div>
+				{/each}
 			</div>
 			<div class="flex w-full h-12 bg-slate-100 items-center">
 				<div class="px-6">
-					<h3 class="font-medium">Features</h3>
+					<h3 class="font-medium">Platform Specifications</h3>
 				</div>
 			</div>
-			<div class="grid grid-cols-3 divide-x h-auto border-b-2 border-b-slate-100">
-				<div class="w-full h-full px-6 py-6 text-start text-gray-500">CPU Architecture</div>
-				<div class="w-full h-full px-6 py-6 text-center">Free</div>
-				<div class="w-full h-full px-6 py-6 text-center">Free</div>
+			{#each Object.entries(specs) as [key, value]}
+				<div class="grid grid-cols-3 divide-x h-auto border-b-2 border-b-slate-100">
+					<div class="w-full h-full px-6 py-2 text-start text-gray-500">
+						{value}
+					</div>
+					{#each results as result}
+						<div class="w-full h-full px-6 py-2 text-center">
+							{result['specs'][key]}
+						</div>
+					{/each}
+				</div>
+			{/each}
+			<div class="flex w-full h-12 bg-slate-100 items-center">
+				<div class="px-6">
+					<h3 class="font-medium">Performance</h3>
+				</div>
 			</div>
+			{#each Object.entries(performance) as [key, value]}
+				<div class="grid grid-cols-3 divide-x h-auto border-b-2 border-b-slate-100">
+					<div class="w-full h-full px-6 py-2 text-start text-gray-500">
+						{value}
+					</div>
+					{#each results as result}
+						<div class="w-full h-full px-6 py-2 text-center">
+							{result['performance'][key]}
+						</div>
+					{/each}
+				</div>
+			{/each}
+			<div class="flex w-full h-12 bg-slate-100 items-center">
+				<div class="px-6">
+					<h3 class="font-medium">Scalability</h3>
+				</div>
+			</div>
+			{#each Object.entries(scalability) as [key, value]}
+				<div class="grid grid-cols-3 divide-x h-auto border-b-2 border-b-slate-100">
+					<div class="w-full h-full px-6 py-2 text-start text-gray-500">
+						{value}
+					</div>
+					{#each results as result}
+						<div class="w-full h-full px-6 py-2 text-center">
+							{result['scalability'][key]}
+						</div>
+					{/each}
+				</div>
+			{/each}
 		</div>
 	</div>
 </div>
