0db17d0f9bf6509f8fa28db20797f2beac2756d6
Hunter Johnston <johnstonhuntera@gmail.com>
2022-09-07 22:20:45 -0400

    environment variable update

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
