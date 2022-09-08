7a44902355c2315bde99e691d0bf7f6442cf3d22
Hunter Johnston <johnstonhuntera@gmail.com>
2022-09-07 22:33:31 -0400

    dynamic dev/prod env variables, added restart to bom

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
+let supabaseUrl;
+let supabaseAnonKey;
+
+if (process.env.VITE_SUPABASE_URL && process.env.VITE_SUPABASE_ANON_KEY) {
+	supabaseUrl = process.env.VITE_SUPABASE_URL;
+	supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
+} else {
+	supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+	supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+}
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
