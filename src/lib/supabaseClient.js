5f27c8e10e24fcdeeb6016b348f73952bbe13751
Hunter Johnston <johnstonhuntera@gmail.com>
2022-09-07 22:19:07 -0400

    updated environment variables for production

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
